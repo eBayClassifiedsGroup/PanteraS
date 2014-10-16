c416f4d59d32aebabe3a033b8bb3aa69dc5d84aa
root <root@paas44-1.mobile.rz>
2014-10-16 17:22:01 +0200

    PaaS: fix that build also paas image

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+docker build --rm=true --tag=paas paas
 docker build --rm=true --tag=mesos mesos
 docker build --rm=true --tag=mesos-slave mesos-slave
 docker build --rm=true --tag=mesos-master mesos-master
