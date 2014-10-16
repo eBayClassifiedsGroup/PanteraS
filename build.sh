cc0491823e38ec6cfeae035ba33b0de3b89ca798
root <root@paas44-1.mobile.rz>
2014-10-16 14:32:10 +0200

    HAproxy fixes

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ docker build --rm=true --tag=mesos mesos
 docker build --rm=true --tag=mesos-slave mesos-slave
 docker build --rm=true --tag=mesos-master mesos-master
 docker build --rm=true --tag=consul consul
+docker build --rm=true --tag=haproxy haproxy
