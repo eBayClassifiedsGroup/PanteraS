25a8181552672e6d10dd86050cc4fd0b2b975553
root <root@docker46-1.mobile.rz>
2014-10-29 14:03:09 +0100

    Fix for genfig.sh to support macOS

--- genfig.sh
+++ genfig.sh
@@ -3,9 +3,6 @@
 DC=${DC:-"UNKNOWN"}
 BOOTSTRAP=${BOOTSTRAP:-" -bootstrap-expect 1"}
 MODE=${MODE:-" -server"}
-
-if [ -z "$IP" ]; then
-IP=$(sudo ip addr show eth0 | perl -nle '/inet ([\d\.]+)/ && print $1')
-fi
+IP=${IP:-$(ifconfig | awk '/inet .*10/{gsub(/.*:/,"",$2);print $2;exit}')}
 
 eval "$(cat fig.yml.tpl| sed 's/"/+++/g'|sed  's/^\(.*\)$/echo "\1"/')"|sed 's/+++/"/g'|sed 's;\\";";g' > fig.yml
