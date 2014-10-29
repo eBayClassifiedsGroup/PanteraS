7e0411ada2249692f70a53e305c108b0520b8c4c
Jonathan Colby <jcolby@team.mobile.de>
2014-10-29 12:11:56 +0100

    figure out IP if none provided

--- genfig.sh
+++ genfig.sh
@@ -4,4 +4,8 @@ DC="UNKNOWN"
 BOOTSTRAP=" -bootstrap-expect 1"
 MODE=" -server"
 
+if [ -z "$IP" ]; then
+IP=$(sudo ip addr show eth0 | perl -nle '/inet ([\d\.]+)/ && print $1')
+fi
+
 eval "$(cat fig.yml.tpl| sed 's/"/+++/g'|sed  's/^\(.*\)$/echo "\1"/')"|sed 's/+++/"/g'|sed 's;\\";";g' > fig.yml
