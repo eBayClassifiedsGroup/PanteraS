ec8450d62716bd2031e241029dce35ac1087d93c
Mateusz Szczap <mateusz.szczap@gmail.com>
2014-10-31 16:31:25 +0100

    makes docker command line tool work outside of boot2docker vm

--- genfig.sh
+++ genfig.sh
@@ -3,6 +3,14 @@
 DC=${DC:-"UNKNOWN"}
 BOOTSTRAP=${BOOTSTRAP:-" -bootstrap-expect 1"}
 MODE=${MODE:-" -server"}
+
+which boot2docker && {
+  boot2docker init
+  boot2docker start
+  $(boot2docker shellinit)
+  HOSTNAME=boot2docker
+}
+[ -z $DOCKER_HOST ] || IP=$(echo $DOCKER_HOST | sed 's;.*//\(.*\):.*;\1;')
 IP=${IP:-$(ifconfig | awk '/inet .*10/{gsub(/.*:/,"",$2);print $2;exit}')}
 
 eval "$(cat fig.yml.tpl| sed 's/"/+++/g'|sed  's/^\(.*\)$/echo "\1"/')"|sed 's/+++/"/g'|sed 's;\\";";g' > fig.yml
