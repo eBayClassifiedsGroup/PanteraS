fd88b47cd880363647f480ebd9c9a89efbc3f318
Wojciech Sielski <sielaq@gmail.com>
2014-11-06 22:12:52 +0100

    Example gets boot2docker IP if exists

--- examples/SimpleWebappPython/start_with_marathon.sh
+++ examples/SimpleWebappPython/start_with_marathon.sh
@@ -1,5 +1,8 @@
+[ -z $DOCKER_HOST ] || IP=$(echo $DOCKER_HOST | sed 's;.*//\(.*\):.*;\1;')
+IP=${IP:-$(ifconfig | awk '/inet .*10/{gsub(/.*:/,"",$2);print $2;exit}')}
+
 echo "Clean up previous example"
-curl -X DELETE -H "Content-Type: application/json" http://localhost:8080/v2/apps/simplewebapp?force=true >/dev/null 2>&1
+curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/simplewebapp?force=true >/dev/null 2>&1
 sleep 1
 echo "Start a new one"
-curl -X POST -H "Content-Type: application/json" http://localhost:8080/v2/apps/ -d@deploy_marathon.json
+curl -X POST -H "Content-Type: application/json" http://${IP}:8080/v2/apps/ -d@deploy_marathon.json
