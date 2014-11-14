4bde754513b230d8f3989146c50bc8fc58ff1498
Jonathan Colby <jcolby@team.mobile.de>
2014-11-14 15:34:23 +0100

    fix env variable name

--- haproxy/haproxy-marathon-bridge-wrapper.sh
+++ haproxy/haproxy-marathon-bridge-wrapper.sh
@@ -7,7 +7,7 @@ while true ; do
 
 if [ -e $SCRIPT ] ; then
  echo "$(date): regenerating haproxy config from marathon API"
- $SCRIPT $ENV_MASTER_IP:8080 > $OUTPUT
+ $SCRIPT $MASTER_IP:8080 > $OUTPUT
 else 
  echo "$(date): cannot find script $SCRIPT"
 fi
