86c3831c043ce314d04b14da18562ad8d19e04f9
Wojciech Sielski <sielaq@gmail.com>
2014-11-28 15:45:55 +0100

    HAproxy changes - correct example file accordingly

--- infrastructure/haproxy/run.sh
+++
@@ -1,8 +0,0 @@
-docker kill haproxy
-docker rm haproxy
-docker run -d -p 80:80 -p 9081:81 -p 9082:82 --name=haproxy \
-  -h haproxy -v /opt:/haproxy-override haproxy \
-  -addr=paas44-1.mobile.rz:8500 \
-  -backend "c=consul@44_Integra_229:8500" \
-  -backend "s=SimplePython@44_Integra_229" \
-  -reload "reload haproxy"
