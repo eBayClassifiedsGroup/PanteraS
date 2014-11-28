c1fab14de8bc7f8f25a906d079a953230097523a
Jonathan Colby <jcolby@team.mobile.de>
2014-11-28 13:49:14 +0100

    restructure image directories

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+PREFIX=infrastructure
 
-docker build --rm=true --tag=paas paas
-docker build --rm=true --tag=mesos mesos
-docker build --rm=true --tag=mesos-slave mesos-slave
-docker build --rm=true --tag=mesos-master mesos-master
-docker build --rm=true --tag=consul consul
-docker build --rm=true --tag=haproxy haproxy
-docker build --rm=true --tag=openvpn openvpn
-docker build --rm=true --tag=registrator registrator
-docker build --rm=true --tag=dnsmasq dnsmasq
+docker build --rm=true --tag=paas $PREFIX/paas
+docker build --rm=true --tag=mesos $PREFIX/mesos
+docker build --rm=true --tag=mesos-slave $PREFIX/mesos-slave
+docker build --rm=true --tag=mesos-master $PREFIX/mesos-master
+docker build --rm=true --tag=consul $PREFIX/consul
+docker build --rm=true --tag=haproxy $PREFIX/haproxy
+docker build --rm=true --tag=openvpn $PREFIX/openvpn
+docker build --rm=true --tag=registrator $PREFIX/registrator
+docker build --rm=true --tag=dnsmasq $PREFIX/dnsmasq
