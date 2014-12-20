e8c389636d73949e66cbcf54b53ae3147f35ad91
Wojciech Sielski <sielaq@gmail.com>
2014-12-20 09:34:34 +0100

    External application versioning

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 PREFIX=infrastructure
 
-docker build --rm=true --tag=paas $PREFIX/paas
-docker build --rm=true --tag=mesos $PREFIX/mesos
-docker build --rm=true --tag=mesos-slave $PREFIX/mesos-slave
-docker build --rm=true --tag=mesos-master $PREFIX/mesos-master
-docker build --rm=true --tag=consul $PREFIX/consul
-docker build --rm=true --tag=haproxy $PREFIX/haproxy
+myexit(){
+ echo "ERROR DURING BUILDING IMAGE"
+ exit 1
+}
+
+docker build --rm=true --tag=paas $PREFIX/paas || myexit
+docker build --rm=true --tag=mesos $PREFIX/mesos || myexit
+docker build --rm=true --tag=mesos-slave $PREFIX/mesos-slave || myexit
+docker build --rm=true --tag=mesos-master $PREFIX/mesos-master || myexit
+docker build --rm=true --tag=consul $PREFIX/consul || myexit
+docker build --rm=true --tag=haproxy $PREFIX/haproxy || myexit
 docker build --rm=true --tag=openvpn $PREFIX/openvpn
-docker build --rm=true --tag=registrator $PREFIX/registrator
-docker build --rm=true --tag=dnsmasq $PREFIX/dnsmasq
+docker build --rm=true --tag=registrator $PREFIX/registrator || myexit
+docker build --rm=true --tag=dnsmasq $PREFIX/dnsmasq || myexit
