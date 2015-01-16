a96e8536ba0f7289de365b735917d7ef0425cd4b
Wojciech Sielski <sielaq@gmail.com>
2015-01-16 11:12:49 +0100

    few changes related to docker and consul-template
    * Docker 1.4.1
    * consul-template 1.5.1 + builing the newest master branch to play
    * change HAproxy config template - since consul-template supports few nice fetures

--- examples/SimpleWebappPython/start_with_marathon.sh
+++ examples/SimpleWebappPython/start_with_marathon.sh
@@ -2,8 +2,8 @@
 IP=${IP:-$(ifconfig | awk '/inet .*10/{gsub(/.*:/,"",$2);print $2;exit}')}
 
 echo "Clean up previous example"
-curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/webapp1?force=true >/dev/null 2>&1
-curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/webapp2?force=true >/dev/null 2>&1
+curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/python1?force=true >/dev/null 2>&1
+curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/python2?force=true >/dev/null 2>&1
 sleep 1
 echo "Start a new one"
 curl -X POST -H "Content-Type: application/json" http://${IP}:8080/v2/apps/ -d@deploy1_marathon.json
