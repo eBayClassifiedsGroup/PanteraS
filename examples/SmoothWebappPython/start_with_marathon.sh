673e10ceb0a1d123246517a71978b8f84fe08bdf
Wojciech Sielski <sielaq@gmail.com>
2015-05-06 15:08:55 +0200

    Introducing canaries + examples

--- examples/SmoothWebappPython/start_with_marathon.sh
+++ examples/SmoothWebappPython/start_with_marathon.sh
@@ -5,8 +5,8 @@ IP=${IP:-$(ifconfig | awk '/inet .*10/{gsub(/.*:/,"",$2);print $2;exit}')}
 if [ ! -n "$1" ]
 then
   echo "No arguments try to clean up previous example"
-  curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/python-example-stable?force=true >/dev/null 2>&1
-  curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/python-example-canaries?force=true >/dev/null 2>&1
+  curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/python-smooth-stable?force=true >/dev/null 2>&1
+  curl -X DELETE -H "Content-Type: application/json" http://${IP}:8080/v2/apps/python-smooth-canaries?force=true >/dev/null 2>&1
 else
   echo "Start a new one"
   curl -X POST -H "Content-Type: application/json" http://${IP}:8080/v2/apps/ -d@$1
--- frameworks/java7/start.sh
+++ frameworks/java7/start.sh
@@ -7,7 +7,7 @@ maintenance(){
     # For each extenal ports you can map internal one from PORT_${int}
     port_int=$(env|sed -n "s/PORT_\([0-9]*\)=$port/\1/p")
     # registartor use ServiceID that contains variables, which are now all available
-    consul_service_id="${HOST}:${CONTAINER_NAME}:${port_int}"
+    consul_service_id="${HOST%%.*}:${CONTAINER_NAME}:${port_int}"
     curl -X PUT "http://${HOST}:8500/v1/agent/service/maintenance/${consul_service_id}?enable=true"
     # if you use udp uncomment also the udp to switch into maintenance mode
     #curl -X PUT "http://${HOST}:8500/v1/agent/service/maintenance/${consul_service_id}:udp?enable=true"
