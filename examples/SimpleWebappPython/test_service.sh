699d1e2d543d8a42e7a7c756cc2c9e67efff8788
Sielski <sielaq@gmail.com>
2015-09-17 11:35:24 +0200

    make Travis test simpler

--- examples/SimpleWebappPython/test_service.sh
+++ examples/SimpleWebappPython/test_service.sh
@@ -1,6 +1,11 @@
 json2yaml deploy0_marathon.json > deploy.yml
-marathon_deploy -e PRODUCTION
 
+#marathon_deploy -e PRODUCTION -u http://$IP:8080
+./start_with_marathon.sh deploy0_marathon.json
+
+sleep 20
+docker images
+docker ps
 for i in {1..20}; do 
   curl --fail -H 'Host: python.service.consul' http://${IP}
 done
