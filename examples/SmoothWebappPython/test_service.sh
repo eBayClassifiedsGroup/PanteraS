8f1b46d39fdaf7ed3077a39a6b7926d9730140fe
Sielski <sielaq@gmail.com>
2015-09-23 09:34:13 +0200

    Check travis

---
+++ examples/SmoothWebappPython/test_service.sh
@@ -0,0 +1,18 @@
+json2yaml deploy0_marathon.json > deploy.yml
+
+./build-docker-image.sh
+#marathon_deploy -e PRODUCTION -u http://$IP:8080
+./start_with_marathon.sh deploy0_marathon.json
+
+sleep 20
+docker images
+docker ps
+for i in {1..20}; do 
+  curl --fail -H 'Host: python-smooth.service.consul' http://${IP}
+done
+
+docker exec -ti panteras_panteras_1 supervisorctl status
+curl -s http://${IP}:8080/v2/leader
+curl -s http://${IP}:8080/v2/info
+curl -I -s http://${IP}:5050/master/health
+curl -s http://${IP}:5050/system/stats.json
