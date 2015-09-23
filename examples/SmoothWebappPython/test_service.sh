3f38a39fce5d2ade5656c528296e488ebbbe847b
Sielski <sielaq@gmail.com>
2015-09-23 18:29:52 +0200

    Check travis

--- examples/SmoothWebappPython/test_service.sh
+++ examples/SmoothWebappPython/test_service.sh
@@ -8,11 +8,5 @@ sleep 20
 docker images
 docker ps
 for i in {1..20}; do 
-  curl --fail -H 'Host: python-smooth.service.consul' http://${IP}
+  curl --fail -H 'Host: python-smooth.service.consul' http://${IP}/cgi-bin/index
 done
-
-docker exec -ti panteras_panteras_1 supervisorctl status
-curl -s http://${IP}:8080/v2/leader
-curl -s http://${IP}:8080/v2/info
-curl -I -s http://${IP}:5050/master/health
-curl -s http://${IP}:5050/system/stats.json
